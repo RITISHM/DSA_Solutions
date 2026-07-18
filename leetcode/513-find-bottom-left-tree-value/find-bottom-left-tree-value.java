72ce4bf7e238b043acab7c5e42851c9587f322ae
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-07-18 21:13:01 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (18.29%) - LeetSync

--- leetcode/513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
+++ leetcode/513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
@@ -1,53 +1,17 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    class Pair{
-        TreeNode node;
-        String side;
-
-        Pair(TreeNode node, String side){
-            this.node =node;
-            this.side=side;
-        }
-    }
-
-
+    private int maxDepth = -1;
+    private int answer = 0;
     public int findBottomLeftValue(TreeNode root) {
-            
-            Queue <TreeNode> levels = new LinkedList<>();
-            int res = -1;
-            levels.offer(root);
-
-            while(!levels.isEmpty()){
-            
-            int size = levels.size();
-
-            for (int i = 1; i <= size; i++){
-
-                TreeNode curr = levels.poll();
-
-                if (i == size ) res = curr.val;
-
-                if (curr.right != null) levels.offer(curr.right);
-                if (curr.left != null) levels.offer(curr.left);
-
-            }
-
-            }
-            
-            return res;
+        dfs(root, 0);
+        return answer;
+    }
+    private void dfs(TreeNode node, int depth) {
+        if (node == null) return;
+        if (depth > maxDepth) {
+            maxDepth = depth;
+            answer = node.val;
+        }
+        dfs(node.left, depth + 1);
+        dfs(node.right, depth + 1);
     }
 }
