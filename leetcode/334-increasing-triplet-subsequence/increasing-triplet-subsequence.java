ed92b9c816e241f452e41738c9e0c8a9d398c207
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-07-26 02:27:05 +0530

    Time: 2 ms (99.48%) | Memory: 122.7 MB (40.31%) - LeetSync

--- leetcode/334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
+++ leetcode/334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -1,38 +1,15 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        if (nums.length < 3) return false ;
-
-        int n = nums.length;
-        int [] nge = new int [n];
-        int [] lse = new int [n];
-        Stack <Integer> st = new Stack<>();
-
-        for (int i = 0; i < n ; i++ ){
-            while (!st.isEmpty() && nums[i] <= st.peek()) st.pop();
-
-            if (st.isEmpty()) lse[i] = Integer.MIN_VALUE;
-            else lse[i] = st.peek();
-            
-            st.push(nums[i]);
+         int a=Integer.MAX_VALUE, b=Integer.MAX_VALUE;
+        for(int i:nums){
+            if(i<=a)
+                a=i;
+            else if(i<=b)
+                b=i;
+            else
+                return true;
         }
-
-        while (!st.isEmpty()) st.pop();
-
-        for (int i = n-1; i >= 0; i-- ){
-            while (!st.isEmpty() && nums[i] >= st.peek()) st.pop();
-
-            if (st.isEmpty()) nge[i] = Integer.MIN_VALUE;
-            else nge[i] = st.peek();
-            
-            st.push(nums[i]);
-        }
-    
-
-        for (int i = 0; i < n; i++){
-            if (lse[i] != Integer.MIN_VALUE && nge[i] != Integer.MIN_VALUE)return true;
-        }
-
-
         return false;
     }
 }
+    
