0fff71123d6661bc61ffcbe84f7fa54b4faeaca3
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-07-28 17:47:35 +0530

    Time: 34 ms (36.95%) | Memory: 48.3 MB (23.69%) - LeetSync

--- leetcode/3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
+++ leetcode/3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -1,68 +1,39 @@
 class Solution {
 
-    void mergeSort(char [] chars, int low, int high){
-        if (low >= high) return ;
-        
-        int mid = (( high - low ) / 2) + low;
-        
-        mergeSort(chars, low, mid);
-        mergeSort(chars, mid+1, high);
-
-        merge(chars, low, mid, high);
-    }
-
-    void merge(char [] chars, int low, int mid, int high){
-
-        int lp = low;
-        int rp = mid+1;
-        char [] temp = new char[ high - low + 1];
-        int tp = 0;
-
-        while( lp <= mid && rp <= high) {
-
-            if (chars[lp] <= chars[rp]) {
-                temp[ tp++ ] = chars[lp++];
-            }
-
-            else {
-                temp[ tp++ ] = chars[rp++];
-            }
-        }
-
-        while( lp <= mid ){
-            temp[ tp++ ] = chars[lp++];
-        }
-
-        while( rp <= high ){
-            temp[ tp++ ] = chars[rp++];
-        }
-
-        tp = 0;
-
-        while (tp < temp.length){
-            chars[low++] = temp[tp++];
-        }
-
-
-    }
+   
 
     public String smallestPalindrome(String s) {
         int len = s.length();
         if (len == 1 || len == 3) return s;
 
-        int low = 0;
-        int high = len - 1;
-        int mid = (len / 2) - 1;
+   
+        StringBuilder sb = new StringBuilder();
 
-        char [] chars = s.toCharArray();
-        mergeSort( chars, low, mid);
-        System.out.println(new String(chars));
+        int [] chars = new int[26];
+        char middle ='a';
 
-        for (int i = 0; i < len / 2; i++){
-            chars[len - 1 - i] = chars[i];
+        for (char c : s.toCharArray()){
+            chars[c - 'a']++;
         }
 
-        return new String(chars);
+        for (int i = 0; i < 26; i++){
+
+            if (chars[i] % 2 == 1) middle = (char) ('a' + i);
+
+            for (int j = 0; j < chars[i] / 2; j++ ){
+                sb.append((char) ('a' + i));
+            }
+        }
+        
+        String firstHalf = sb.toString();
+        String secondHalf = sb.reverse().toString();
+
+        
+        if (len % 2 != 0 && middle != '\0') {
+            return firstHalf + middle + secondHalf;
+        }
+
+        return firstHalf + secondHalf;
 
     }
 }
