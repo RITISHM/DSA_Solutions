14fab31d5dfefe5b2b4e6ee1fc910fe97d406c84
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-07-28 17:57:48 +0530

    Time: 23 ms (83.53%) | Memory: 48 MB (75.90%) - LeetSync

--- leetcode/3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
+++ leetcode/3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -10,30 +10,30 @@ class Solution {
         StringBuilder sb = new StringBuilder();
 
         int [] chars = new int[26];
-        char middle ='a';
+        char [] res = new char[len];
+
+        int left = 0;
+        int right = len -1;
+        int curr = 0;
 
         for (char c : s.toCharArray()){
             chars[c - 'a']++;
         }
+        
+        for (int i = 0; i < 26; i++) {
+            if (chars[i] == 0) continue;
 
-        for (int i = 0; i < 26; i++){
 
-            if (chars[i] % 2 == 1) middle = (char) ('a' + i);
-
-            for (int j = 0; j < chars[i] / 2; j++ ){
-                sb.append((char) ('a' + i));
+            if (chars[i] % 2 != 0){
+                res[len/2] = (char)('a' + i);
             }
+
+            for (int j = 0; j < chars[i] / 2; j++){
+                res[left++] = (char) ('a' + i);
+                res[right--] =  (char) ('a' + i);
+            }
+           
         }
-        
-        String firstHalf = sb.toString();
-        String secondHalf = sb.reverse().toString();
-
-        
-        if (len % 2 != 0 && middle != '\0') {
-            return firstHalf + middle + secondHalf;
-        }
-
-        return firstHalf + secondHalf;
-
+        return new String(res);
     }
 }
