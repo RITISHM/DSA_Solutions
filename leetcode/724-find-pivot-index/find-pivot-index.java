456ec84bc166ef06658ef3ce5af24283ff0cb423
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-07-30 16:56:09 +0530

    Time: 1 ms (97.52%) | Memory: 47.5 MB (41.05%) - LeetSync

--- leetcode/724-find-pivot-index/find-pivot-index.java
+++ leetcode/724-find-pivot-index/find-pivot-index.java
@@ -1,19 +1,13 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int [] prefix = new int [nums.length];
-        int [] postfix = new int [nums.length];
-        int presum = 0;
-        int postsum = 0;
-        int lastIndex = nums.length - 1;
-        for (int i = 0; i < nums.length; i++){
-            presum += nums[i];
-            prefix[i] = presum;
-            postsum += nums[lastIndex - i];
-            postfix[lastIndex - i] = postsum;
-        }
+        int sum = 0;
+        int prefix = 0; 
+        for (int i = 0; i < nums.length; i++) sum += nums[i];
 
         for (int i = 0; i < nums.length; i++){
-            if (postfix[i] == prefix[i]) return i;
+            if ( ((double)(sum - nums[i])) / 2 == (double) prefix ) return i;
+            prefix += nums[i];
+
         }
 
         return -1;
