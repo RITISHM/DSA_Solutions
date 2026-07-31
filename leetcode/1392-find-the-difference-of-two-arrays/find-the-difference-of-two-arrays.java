8af641955d9a6a2e94fe4c68c51ca7da03174574
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-07-31 17:12:51 +0530

    Time: 10 ms (92.19%) | Memory: 47.6 MB (39.05%) - LeetSync

--- leetcode/1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
+++ leetcode/1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.java
@@ -2,26 +2,23 @@ class Solution {
     public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
         HashSet <Integer> a = new HashSet();
         HashSet <Integer> b = new HashSet();
+        List<List<Integer>> res= new ArrayList<>();
+        res.add( new ArrayList<>());
+        res.add( new ArrayList<>());
 
         for (int i : nums1) a.add(i);
         for (int i : nums2) b.add(i);
 
-
-        for(int i = 0; i < nums2.length; i++){
-            if (a.contains(nums2[i])) a.remove(nums2[i]);
+        for(int i : a){
+            if (!b.contains(i)) res.get(0).add(i);
         }
 
-        for(int i = 0; i < nums1.length; i++){
-            if (b.contains(nums1[i])) b.remove(nums1[i]);
+        for(int i : b){
+            if (!a.contains(i)) res.get(1).add(i);
         }
-        
-        List <Integer> res1 = new ArrayList<>(a);
-        List <Integer> res2 = new ArrayList<>(b);
 
-        List<List<Integer>> res = new ArrayList<>();
-        res.add(res1);
-        res.add(res2);
         
+
         return res;
     }
 }
