cbc9cf1259ef8e4ced88a263e479fc41c16354a6
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-08-04 19:29:34 +0530

    Time: 159 ms (5.05%) | Memory: 47 MB (14.07%) - LeetSync

---
+++ leetcode/23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode merge(ListNode n1, ListNode n2){
+
+    ListNode newHead = new ListNode();
+    ListNode curr = newHead;
+    ListNode ptr1 = n1;
+    ListNode ptr2 = n2;
+
+    while(ptr1 != null && ptr2 != null){
+      if(ptr1.val < ptr2.val){
+        curr.next = ptr1;
+        ptr1 = ptr1.next;
+        curr = curr.next;
+      }
+
+      else{
+        curr.next = ptr2;
+        ptr2 = ptr2.next;
+        curr = curr.next;
+      }
+    }
+
+    while (ptr1 != null){
+      curr.next = ptr1;
+      ptr1 = ptr1.next;
+      curr = curr.next;
+    }
+
+    while (ptr2 != null){
+      curr.next = ptr2;
+      ptr2 = ptr2.next;
+      curr = curr.next;
+    }
+
+    return newHead.next;
+
+  }
+
+  public ListNode mergeKLists(ListNode [] lists){
+    if (lists.length == 0) return null;
+    ListNode a = lists[0];
+    
+    for (int i = 1; i < lists.length; i++){
+      ListNode b = lists[i];
+      a = merge(a, b);
+    }
+
+    return a;
+
+  }
+}
