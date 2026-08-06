2e257506de16d10fd4c95c468af246b420bae237
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-08-06 15:26:05 +0530

    Time: 10 ms (98.98%) | Memory: 58.5 MB (23.93%) - LeetSync

--- leetcode/1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ leetcode/1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,35 +1,31 @@
 class Solution {
 
-    public void swap(int[] arr, int i, int j){
-        int temp = arr[i];
+    public void swap(double [][] arr, int i, int j){
+        double [] temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
-    double distance(int index , int[][] points){
-        int x = points[index][0];
-        int y = points[index][1];
-        return Math.sqrt((x * x) + (y * y));
-    }
 
-    public int quickSelect(int L, int R,int [] orignalIndex, int[][] points){
+    public int quickSelect(int L, int R, double[][] arr){
+
         int randomIndex = L + (int)(Math.random() * (R - L + 1));
-        swap(orignalIndex, L, randomIndex);
+        swap(arr, L, randomIndex);
         int pivotIndex = L;
         int i = L + 1;
         int j = R;
 
         while(i <= j){
-            while(i <= R && distance(orignalIndex[i], points) <= distance(orignalIndex[pivotIndex],points)) i++;
-            while(j >= L - 1 && distance(orignalIndex[j], points) > distance(orignalIndex[pivotIndex],points)) j--;
+            while(i <= R && arr[i][0] <= arr[pivotIndex][0]) i++;
+            while(j >= L - 1 && arr[j][0] > arr[pivotIndex][0]) j--;
             if(i < j){
-                swap(orignalIndex, i , j);
+                swap(arr, i , j);
                 i++;
                 j--;
             }
 
         }
 
-        swap(orignalIndex, pivotIndex, j);
+        swap(arr, pivotIndex, j);
         return j;
     
     }
@@ -37,25 +33,33 @@ class Solution {
 
     public int[][] kClosest(int[][] points, int k) {
         if(k == points.length) return points;
-        int[] orignalIndex = new int [points.length];
-        for(int i = 0; i < points.length; i++) orignalIndex[i] = i;
 
+        double [][] distanceArr = new double [points.length][2];
+
+        for (int i = 0; i < distanceArr.length; i++){
+            int [] point = points[i];
+            int x = point[0];
+            int y = point[1];
+            double distance = Math.sqrt((x*x) + (y*y));
+            distanceArr[i][0] = distance;
+            distanceArr[i][1] = i;
+        }
         int L = 0;
-        int R = points.length - 1;
-        int pivotIndex = quickSelect(L, R, orignalIndex, points);
-        while(pivotIndex != k){
+        int R = distanceArr.length - 1;
+        int pivotIndex = quickSelect(L, R, distanceArr);;
+        while(pivotIndex != k ){
             if(pivotIndex < k){
                 L = pivotIndex + 1;
             }
             else{
                 R = pivotIndex - 1;
             }
-            pivotIndex = quickSelect(L, R, orignalIndex, points);
+            pivotIndex = quickSelect(L, R, distanceArr);
         }
 
         int [][] res = new int [k][2];
         for(int i = 0; i < k; i++){
-            int index = orignalIndex[i];
+            int index = (int) distanceArr[i][1];
             res[i] = points[index];
         }
 
