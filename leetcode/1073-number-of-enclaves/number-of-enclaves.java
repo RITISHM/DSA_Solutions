0f9012f148a74e2084d32c1b16c2c2432d06f425
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-08-07 01:18:15 +0530

    Time: 9 ms (50.63%) | Memory: 53.1 MB (90.09%) - LeetSync

--- leetcode/1073-number-of-enclaves/number-of-enclaves.java
+++ leetcode/1073-number-of-enclaves/number-of-enclaves.java
@@ -26,7 +26,7 @@ class Solution {
     public int numEnclaves(int[][] grid) {
         int rows = grid.length;
         int cols = grid[0].length;
-        Queue <int []> nodes = new LinkedList<>();
+        Queue <int []> nodes = new ArrayDeque<>();
         int count = 0;
 
         
@@ -36,11 +36,11 @@ class Solution {
                     if(row == 0 || row == (rows - 1) || col == 0 || col == (cols - 1)){
                         grid[row][col] = 0;
                         nodes.offer(new int []{row, col});
+                        bfs(nodes, grid);
                     }
                 }
             }
         }
-        bfs(nodes, grid);
 
         for(int row = 0; row < rows; row++){
             for(int col = 0; col < cols; col++){
