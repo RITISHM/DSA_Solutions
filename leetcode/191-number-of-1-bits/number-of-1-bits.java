15ae37b30fa8ca7c4da2aea41b6e96c7fdce6d39
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-08-10 05:19:30 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (28.95%) - LeetSync

---
+++ leetcode/191-number-of-1-bits/number-of-1-bits.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        for(int i = 0 ; i < 31; i++){
+            if(n % 2 == 1)count ++;
+            n >>= 1;
+        }
+        return count;
+    }
+}
