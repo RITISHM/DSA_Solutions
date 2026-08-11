6e1e6df64c8ece80084bd8df2cb30e2737f54556
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-08-11 18:11:29 +0530

    Time: 1 ms (97.42%) | Memory: 47.5 MB (62.82%) - LeetSync

--- leetcode/73-set-matrix-zeroes/set-matrix-zeroes.java
+++ leetcode/73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -19,20 +19,12 @@ class Solution {
         }
 
         for(int row = 1; row < rows; row++){
-            if(matrix[row][0] == 0){
-                for(int col = 0; col < cols; col++){
-                    matrix[row][col] = 0;
-                }
+                for(int col = 1; col < cols; col++){
+                    if(matrix[row][0] == 0 || matrix[0][col] == 0 )matrix[row][col] = 0;
             }
         }
 
-        for(int col = 1; col < cols; col++){
-            if(matrix[0][col] == 0){
-                for(int row = 0; row < rows; row++){
-                    matrix[row][col] = 0;
-                }
-            }
-        }
+        
         if(flag1){
             for(int col = 0; col < cols; col++){
                 matrix[0][col] = 0;
