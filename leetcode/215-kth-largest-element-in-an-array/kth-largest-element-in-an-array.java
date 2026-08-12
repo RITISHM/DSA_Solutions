8052decc1607ce68fa3f571ac54229dc0d3f77cb
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-08-13 04:33:16 +0530

    Time: 13 ms (96.93%) | Memory: 76.1 MB (10.23%) - LeetSync

---
+++ leetcode/215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -0,0 +1,39 @@
+class Solution {
+    void swap(int i, int j, int [] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    int quickSelect(int L, int R, int [] nums){
+        int pivot = L;
+        int i = L+1;
+        int j = R;
+        while(i <= j){
+            while(i <= R && nums[pivot] < nums[i]) i++;
+            while(j >= (L + 1) && nums[pivot] > nums[j]) j--;
+
+            if(i <= j) {
+                swap(i, j, nums);
+                i++;
+                j--;
+            }
+        }
+        swap(L , j, nums);
+        return j;
+    }
+
+    public int findKthLargest(int[] nums, int k) {
+        int L = 0;
+        int R = nums.length - 1;
+        int pivot = quickSelect(L , R, nums);
+
+        while (pivot != (k-1)){
+            if(pivot > (k - 1)) R = pivot - 1;
+            else L = pivot + 1;
+            pivot = quickSelect(L, R, nums);                
+        }
+
+        return nums[pivot];
+    }
+}
