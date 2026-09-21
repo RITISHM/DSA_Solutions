b8f669189c82df4ceb7daabeb61168346001672c
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-09-21 10:00:07 +0530

    Sync GFG - Minimum Multiplications to reach End (Medium)

--- GFG/Medium/0_MinimumMultiplicationsToReachEnd.java
+++ GFG/Medium/0_MinimumMultiplicationsToReachEnd.java
@@ -6,21 +6,25 @@
 
 class Solution {
     public int minSteps(int[] arr, int start, int end) {
-        int[] dist = new int[1000];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        Queue<int[]> q = new LinkedList<>();
+        int[] ans = new int[1000];
+        Arrays.fill(ans, Integer.MAX_VALUE);
+        Queue <int[]> q = new LinkedList<>();
         
-        q.add(new int[]{start, 0});
-        dist[start] = 0;
+        q.add(new int[] {start,0});
+        ans[start] = 0;
+        
         while(!q.isEmpty()){
-            int[] node = q.poll();
-            if(node[1] > dist[node[0]]) continue;
-            if(node[0] == end) return node[1];
+            int[] curr = q.poll();
+            int num = curr[0];
+            int mult = curr[1];
+            if (ans[num] < mult) continue;
+            if (num == end) return ans[num];
             for (int i : arr){
-                int newSol = (node[0] * i) % 1000;
-                if (dist[newSol] > node[1]+1){
-                    dist[newSol] = node[1]+1;
-                    q.add(new int[] {newSol, node[1]+1});
+                int nextNum = (num * i) %1000;
+                int nextMult = mult + 1;
+                if(ans[nextNum] > nextMult){
+                    ans[nextNum] = nextMult;
+                    q.add(new int[] {nextNum, nextMult});
                 }
             }
         }
